6b9f3aa67e7d6dfb16202e5c6d333e287111f346
Digant <digantpatel92@gmail.com>
2024-06-30 15:03:38 +0530

    file processing implemented

---
+++ src/config/fileConfig.js
@@ -0,0 +1,15 @@
+const fileConfig = {
+  LIMIT_UNEXPECTED_FILE: "LIMIT_UNEXPECTED_FILE",
+  MULTIPLE_FILES_ERROR_MESSAGE:
+    "Multiple files not allowed. Please select only one file.",
+  FILE_TYPE_TEXT: "text/plain",
+  FILE_TYPE_ERROR_MESSAGE:
+    "This type of file not allowed. Please select text file.",
+  FILE_NOT_FOUND_ERROR_MESSAGE: "File not found.",
+  SERVER_ERROR_MESSAGE:
+    "Something went wrong in file processing. Please try again.",
+  FILE_UPLOAD_ERROR_MESSAGE:
+    "Something went wrong in file uploading. Please try again.",
+};
+
+module.exports = fileConfig;
--- src/controllers/fileController.js
+++ src/controllers/fileController.js
@@ -1,12 +1,70 @@
-const upload = async (req, res) => {
-  console.log("File uplaod controller....");
+const multer = require("multer");
+const fileUpload = multer().single("file");
+const fileConfig = require("../config/fileConfig");
+const fileService = require("../services/fileService");
+const MAX_FILE_SIZE_ALLOWED_BYTES = 2000000; // 2 MB -> 1000 bytes = 1kb, 1000000 = 1MB
 
-  res.status(200).json({
-    status: "success",
-    message: "File uploaded successfully!",
+const statistics = async (req, res) => {
+  fileUpload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      console.log("Multer ERROR: ", err);
+      if (err.code === fileConfig.LIMIT_UNEXPECTED_FILE) {
+        res.status(400).json({
+          message: fileConfig.MULTIPLE_FILES_ERROR_MESSAGE,
+        });
+        return;
+      }
+    } else if (err) {
+      // An unknown error occurred when uploading.
+      console.log("Error: ", error);
+      res.status(500).json({
+        message: fileConfig.FILE_UPLOAD_ERROR_MESSAGE,
+      });
+      return;
+    }
+
+    try {
+      const file = req.file;
+      // validate if file is present
+      if (!file) {
+        res.status(400).json({
+          message: fileConfig.FILE_NOT_FOUND_ERROR_MESSAGE,
+        });
+        return;
+      }
+      // validate file type
+      if (file?.mimetype !== fileConfig.FILE_TYPE_TEXT) {
+        res.status(400).json({
+          message: fileConfig.FILE_TYPE_ERROR_MESSAGE,
+        });
+        return;
+      }
+      // validate file size
+      if (file?.size > MAX_FILE_SIZE_ALLOWED_BYTES) {
+        res.status(400).json({
+          message: `File size should be less than ${
+            MAX_FILE_SIZE_ALLOWED_BYTES / 1000
+          } KB.`,
+        });
+        return;
+      }
+
+      const stats = fileService.textStatistics(file?.buffer?.toString());
+
+      res.status(200).json({
+        status: "success",
+        data: stats,
+      });
+    } catch (error) {
+      console.log("Error: ", error);
+      res.status(500).json({
+        message: fileConfig.SERVER_ERROR_MESSAGE,
+      });
+      return;
+    }
   });
 };
 
 module.exports = {
-  upload,
+  statistics,
 };
